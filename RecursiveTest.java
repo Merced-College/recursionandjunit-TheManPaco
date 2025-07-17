3c157608259e900a8e54da558c4479eba043f816
CiscoVaz <saberassasin21@gmail.com>
2025-07-17 21:10:47 +0000

    Tests are done being added before testing

--- RecursiveTest.java
+++ RecursiveTest.java
@@ -24,6 +24,30 @@ public class RecursiveTest {
         assertEquals(2, RecursionProblems.countHi("hihi"));
     }
     
-    
+    @Test
+    public void testCountHi2() {
+        assertEquals(1, RecursionProblems.countHi2("ahixhi"));
+        assertEquals(2, RecursionProblems.countHi2("ahibhi"));
+        assertEquals(0, RecursionProblems.countHi2("xhixhi"));
+        assertEquals(1, RecursionProblems.countHi2("xhixhibhi"));
+        assertEquals(1, RecursionProblems.countHi2("hi"));
+    }
 
+    @Test
+    public void testStrCount() {
+        assertEquals(2,Main.strCount("catcowcat","cat"));
+        assertEquals(1,Main.strCount("catcowcat","cow"));
+        assertEquals(0,Main.strCount("catcowcat","dog"));
+        assertEquals(1,Main.strCount("aaa","aa"));
+        assertEquals(1,Main.strCount("abc", "abc"));
+    }
+
+    @Test
+    public void testStringClean() {
+        assertEquals("yza",Main.stringClean("yyzzza"));
+        assertEquals("abcd",Main.stringClean("abbbcdd"));
+        assertEquals("Helo",Main.stringClean("Hello"));
+        assertEquals("a",Main.stringClean("aaaaa"));
+        assertEquals("abc",Main.stringClean("abc"));
+    }
 }
